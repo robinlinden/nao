0c93fed5baca60de527ccaa0affe24eab3afcf14
Robin Linden <dev@robinlinden.eu>
2026-05-23 00:10:36 +0200

    Get rid of default comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,6 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
             )
         }
     }
